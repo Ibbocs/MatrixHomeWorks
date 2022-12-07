ae1d1fab9a1def4e92bb53ce65e67a9b4a66525f
Ibbocs <99998326+Ibbocs@users.noreply.github.com>
2022-12-07 20:27:01 +0400

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
