8e52cd59fe5114494bf54939fda40cbe68ce8dc1
Ibbocs <99998326+Ibbocs@users.noreply.github.com>
2022-12-23 21:59:44 +0400

    Async task1

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //buu thread icinde delegate cixan ggostere bilersiz??
+            //thread niye hansi veziyyetlerde isletmelin olaram da?
+            //thread moterezesinde overload eleyir? main gonderende ele bir sey yazdi
+            //adi normal bir delegate yaradib onu new thread gondermek  olarmi?
+            // burda makeTeaAsync yazanda niye alti yasil olurdu?
+
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            Console.WriteLine($"Start Program");
+            var task1 = MethodAAsync();
+            var task2 = MethodBAsync();
+            var task = Task.Run(async () => await MethodAAsync());
+            var task4 = Task.Run(async () => await MethodBAsync());
+            watch.Stop();
+            Console.WriteLine($"Execution Time:{watch.ElapsedMilliseconds} ms");
+            Console.ReadKey();
+
+            //var watch = new System.Diagnostics.Stopwatch();
+            //watch.Start();
+            //Console.WriteLine($"Start Program");
+            //MethodA();
+            //MethodB();
+            //watch.Stop();
+            //Console.WriteLine($"Execution Time:{watch.ElapsedMilliseconds} ms");
+            //Console.ReadKey();
+
+
+            //var task = Task.Run(async () => await MakeTeaAsync());
+            //Console.WriteLine(task.Result);
+
+        }
+
+        static async Task< int> MethodAAsync()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($" A{i}");
+
+                await Task.Delay(500);
+                
+            }
+            //var res = await MethodBAsync();
+
+            int result = 123;
+            Console.WriteLine($" A returns result {result}");
+            return result;
+        }
+        static async Task<int> MethodBAsync()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($" B{i}");
+                await Task.Delay(500);
+                
+            }
+           // var res = await MethodAAsync();
+            int result = 456;
+            Console.WriteLine($" B returns result {result}");
+            return result;
+        }
+
+
+
+
+
+        static int MethodA()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($" A{i}");
+                Task.Delay(500).Wait();
+            }
+            int result = 123;
+            Console.WriteLine($" A returns result {result}");
+            return result;
+        }
+        static int MethodB()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($" B{i}");
+                Task.Delay(500).Wait();
+            }
+            int result = 456;
+            Console.WriteLine($" B returns result {result}");
+            return result;
+        }
+
+
+
+
+        //public static async Task<string> MakeTeaAsync() 
+        //{
+        //    var water = BoilWaterAsync();
+        //    Console.WriteLine("Get cup");
+        //    Console.WriteLine("Put tea in cups");
+        //    Console.WriteLine(Thread.CurrentThread.ManagedThreadId.ToString()); 
+        //    //yuxaridaki sadece hansi thread islediyini gosterir
+        //    var boil = await water;
+        //    Console.WriteLine("add"+boil);
+        //    return "tea";
+
+        //}
+
+        //public static async Task<string> BoilWaterAsync()
+        //{
+        //    Console.WriteLine("Start kettle");
+        //    await Task.Delay(1000);
+        //    Console.WriteLine(Thread.CurrentThread.ManagedThreadId.ToString());
+        //    Console.WriteLine("Finished");
+        //    return "Water";
+        //}
+    }
+}
--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.ConstrainedExecution;
-using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Delegate
 {
@@ -16,6 +15,14 @@ namespace Delegate
             //OperationDelegate opd = new OperationDelegate(Add); bele nece islede bilerik
             //delegate moterezesindeki int falan sadece  yaazilis ucundu yoxsa hesablamada isletmek olar?
             //bizimm motereze tapsiriginda (((()))) bu niye duzdu ki? acilib baglanmayanlar var axi
+
+            //global value isletmekle ref yazmagin ferqi nedir? cvb ref copyalanma olmadan value type ref. kimi davranmasini eleyir. global valueni her methoda vermek istmeye bilerik deye elverisli olacaq
+            //clas icinde class yaratmaq neye lazimdi ki?
+            //kodun nece saniyey islediyine, harda ne bas verdiyine nece baxa bilerik
+            //garbage collector ancaq heapdaki seyleri silir? stackden hec ne silmir? cvb heap, stack onsuzda scope bagalanda silinir
+            //men ytda bele bir sey gordum ki, GC 1 defe ise dusenden sora sag qalani gen0 atir, bu duz fikirdi? siz ilk yaranan kimi gen0 dusur demisdiz deyesen cvb yt duz deyib
+            //generation sonsuz saya qeder gedir? yoxsa yalniz 3 merhelesi var cvb 3 merhelesi var
+            //exception butun xetalari ozunde saxlayirsa, biz onun sayesinde ele her bir xetani gore bilerik yoxsa o biri alt sinifleri de mutleq isletmeliyik? cvb ozel seyler ucun o biri clasdari isledirik
 
             #region Task1
             Console.WriteLine(Applay(4.2F, 3, Add));
@@ -51,7 +58,7 @@ namespace Delegate
             Console.WriteLine($"Info 2: {ayfon.model}, {ayfon.pathYear}, {ayfon.beginPrices}, {ayfon.nowPrice}");
             #endregion
             #region Task5
-            List<string>user= new List<string>();
+            List<string> user = new List<string>();
             user.Add("Qasim Qanixli");
             user.Add("Veli Sebiyev");
 
@@ -66,32 +73,39 @@ namespace Delegate
 
         public delegate float OperationDelegate(float a, float b);
 
-        static float Add(float _a, float _b) 
-        { 
-        return _a+ _b;
-        }
-        static float Subtract(float _a, float _b) 
+        static float Add(float _a, float _b)
         {
-        return _a - _b;
+            return _a + _b;
         }
-        static float Multiply(float _a, float _b) 
-        { 
-        return _a * _b;
-        }
-        static float Divide(float _a, float _b) 
+        static float Subtract(float _a, float _b)
         {
-        return _a/ _b;
+            return _a - _b;
         }
-        static float Applay(float _a, float _b, OperationDelegate method ) 
+        static float Multiply(float _a, float _b)
+        {
+            return _a * _b;
+        }
+        static float Divide(float _a, float _b)
+        {
+            return _a / _b;
+        }
+        static float Applay(float _a, float _b, OperationDelegate method)
         {
             Console.WriteLine("Your Calculation:");
             return method.Invoke(_a, _b);
         }
 
+
+
         public delegate void CatDelegate(int _value);
         static void CatMethod(CatDelegate catDelegate)
         {
             catDelegate.Invoke(34);
+        }
+
+        public static void she()
+        {
+
         }
 
         public delegate void PersonDel();
