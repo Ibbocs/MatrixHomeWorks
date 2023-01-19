d9d152875a52059419fa8b38aa827833f347e16f
Ibbocs <99998326+Ibbocs@users.noreply.github.com>
2023-01-19 21:19:39 +0400

    BankChallenge

---
+++ BankChallenge/CheckingAcct.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankChallenge
+{
+    internal class CheckingAcct 
+    {
+        public string AccountOwner;
+        public Decimal Balance;
+
+        public CheckingAcct(string name, string surName, Decimal _balance)
+        {
+            Balance = _balance;
+            AccountOwner = name+" "+ surName;
+        }
+
+        public void Deposit(Decimal _deposit)
+        {
+            Balance += _deposit;
+        }
+
+        public void Withdraw(Decimal _withdraw)
+        {
+            if (Balance>=_withdraw)
+            {
+                Balance -= _withdraw;
+            }
+            else
+            {
+                Balance -= (_withdraw + 35);
+            }
+        }
+    }
+}
---
+++ BankChallenge/SavingsAcct.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankChallenge
+{
+    internal class SavingsAcct
+    {
+        public string AccountOwner;
+        public Decimal Balance, Interest;
+        int counter = 0;
+
+        public SavingsAcct(string name,string surname, Decimal _interest, Decimal _balance)
+        {
+            AccountOwner = name+ " "+ surname;
+            Balance = _balance;
+            Interest = _interest;
+        }
+
+        public void Deposit(Decimal _deposit)
+        {
+            Balance += _deposit;
+        }
+
+        public void Withdraw(Decimal _withdraw)
+        {
+            if (Balance>= _withdraw)
+            {
+                counter++;
+                if (counter <4)
+                {
+                    Balance -= _withdraw;
+                }
+                else
+                {
+                    Balance -= (_withdraw + 2);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance :)");
+            }
+        }
+
+        public void ApplyInterest()
+        {
+            Balance = Balance + (Balance * Interest);
+        }
+    }
+}
